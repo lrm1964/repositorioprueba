bbf568f2101c678cdefbc82289236aa80faf9d75
ADMIN <ADMIN@192.168.0.6>
2018-09-06 21:59:44 -0300

    Prueba con primer repositorio

---
+++ src/Camisa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Camisa {
+    
+    String marca;
+    String modelo;
+    int talle;
+    String color;
+    String tela;
+
+    public void getMarca(){
+    
+    }
+    
+    public void getModelo(){
+    
+    }
+    
+    public void getTalle(){
+    
+    }
+    
+    public void getColor(){
+    
+    }
+    
+    public void getTela(){
+    
+    }
+    
+    public void setMarca(){
+    
+    }
+    
+    public void setModelo(){
+    
+    }
+    
+    public void setTalle(){
+    
+    }
+    
+    public void setColor(){
+    
+    }
+    
+    public void setTela(){
+    
+    }
+}
---
+++ src/Zapatilla.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Zapatilla {
+    
+    String marca;
+    String modelo;
+    int talle;
+    String color;
+    String tipo;
+    
+    public void getMarca(){
+    
+    }
+    
+    public void getModelo(){
+    
+    }
+    
+    public void getTalle(){
+    
+    }
+    
+    public void getColor(){
+    
+    }
+    
+    public void getTipo(){
+    
+    }
+    
+    public void setMarca(){
+    
+    }
+    
+    public void setModelo(){
+    
+    }
+    
+    public void setTalle(){
+    
+    }
+    
+    public void setColor(){
+    
+    }
+    
+    public void setTipo(){
+    
+    }
+    
+}
